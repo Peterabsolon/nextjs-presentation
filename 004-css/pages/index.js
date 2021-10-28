f3c872db849b4a458be41a81dbe585676ba30df6
Peter <peterabsolon@yahoo.com>
2021-10-28 19:22:04 +0200

    css

---
+++ 004-css/pages/index.js
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import styles from './index.module.css'
+
+export default () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1 className={styles.hello}>Home</h1>
+      <div>Count: {count}</div>
+      <button onClick={() => setCount(count + 1)}>Click</button>
+    </div>
+  )
+}
